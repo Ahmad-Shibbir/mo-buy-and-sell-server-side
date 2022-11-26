6b8a738cb29d15416e149f634ffafec02182208e
Ahmad-Shibbir <ahmadshibbir.iium@gmail.com>
2022-11-27 05:08:00 +0800

    mongoDb connected

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const cors = require('cors');
+const {MongoClient, ServerApiVersion}= require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASS}@cluster0.btevgbq.mongodb.net/?retryWrites=true&w=majority`;
+
+// const uri =localhost/27017;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const categoryCollection = client.db('mo-buy&sell').collection('cetegory');
+
+        app.get('/category',async(req,res)=>{
+            const query = {};
+           const categories = await categoryCollection.find(query).toArray(); 
+           res.send(categories);
+
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.log);
+
 
 
 app.get('/', async(req, res)=>{
